eb1eaf8f0f913c2baab1f2f6431f0b35e04fe6ba
danielemurgioni <dashomew@gmail.com>
2025-04-03 22:25:40 +0200

    [UPD] with the use of cycle FOR i stamp the email multiple times

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) =>{
     //inserisco all'interno del DOM l'email datami dall'API scelta
-    const emailContainer = document.getElementById("email");
+    //prendo l'elemento dal DOM
+    const emailContainer = document.getElementById("email-container")
+    // dato dell'API
     email = resp.data.response;
-    emailContainer.innerHTML = email;
+    //ciclo for per stampare nel DOM piu email
+    for(let i=0; i<10; i++){
+        //dato dell'API
+        emailHtml = `<div id="email">${email}</div>`
+        emailContainer.innerHTML += emailHtml
+    }
 })
