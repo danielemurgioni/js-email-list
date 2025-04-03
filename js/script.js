05c10475e55633975cdadb33301ba32ca2bc2bed
danielemurgioni <dashomew@gmail.com>
2025-04-03 22:31:38 +0200

    [UPD] Made all emails random with a new for

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) =>{
-    //inserisco all'interno del DOM l'email datami dall'API scelta
-    //prendo l'elemento dal DOM
-    const emailContainer = document.getElementById("email-container")
-    // dato dell'API
-    email = resp.data.response;
-    //ciclo for per stampare nel DOM piu email
-    for(let i=0; i<10; i++){
-        //dato dell'API
+//prendo l'elemento dal DOM
+const emailContainer = document.getElementById("email-container")
+
+//inserisco il tutto dentro un FOR per creare email differenti
+for(let i=0; i<10; i++){
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) =>{
+        // dato dell'API
+        email = resp.data.response;
+        // tag HTML
         emailHtml = `<div id="email">${email}</div>`
+        // inserisco nel DOM il tag HTML
         emailContainer.innerHTML += emailHtml
-    }
-})
+    })
+}
