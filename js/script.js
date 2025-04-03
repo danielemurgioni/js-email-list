acf536fd92f06bf12be87d7eb6e648f8ab24e146
danielemurgioni <dashomew@gmail.com>
2025-04-03 21:31:41 +0200

    [UPD] Selected DOM element by ID and stamp the API email inside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) =>{
-    console.log(resp.data.response)
+    //inserisco all'interno del DOM l'email datami dall'API scelta
+    const emailContainer = document.getElementById("email");
+    email = resp.data.response;
+    emailContainer.innerHTML = email;
 })
